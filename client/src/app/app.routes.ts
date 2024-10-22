24f78a12f36ea66e8b236d0a082745a1c4b9a57c
Abraham Muro <geowall32@gmail.com>
2024-10-22 10:57:35 -0600

    Parcial02 - Add dummy route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,9 +8,16 @@ import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "members", component: MembersListComponent, canActivate: [authGuard]},
-    {path: "members/:id", component: MembersDetailComponent},
-    {path: "list", component: ListsComponent},
-    {path: "messages", component: MessagesComponent},
+    {
+        path: "",
+        runGuardsAndResolvers: "always",
+        canActivate: [authGuard],
+        children:[
+            {path: "members", component: MembersListComponent, canActivate: [authGuard]},
+            {path: "members/:id", component: MembersDetailComponent},
+            {path: "list", component: ListsComponent},
+            {path: "messages", component: MessagesComponent},
+        ]
+    },
     {path: "**", component: HomeComponent, pathMatch: "full"},
 ];
