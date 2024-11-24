15ed1fd9a983e5c6b33b3ca293c926ebc0a53130
Abraham Muro <geowall32@gmail.com>
2024-11-24 12:57:15 -0600

    Parcial03 - Add Membes support

--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-members-list',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './members-list.component.html',
   styleUrl: './members-list.component.css'
 })
-export class MembersListComponent {
+export class MembersListComponent implements OnInit{
+  private memberService = inject(MembersService);
+  members: Member[] = []
 
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members
+    })
+  }
+  
 }
