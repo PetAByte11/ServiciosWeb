625e0f7bbb08f97390cb91d78f3bda47be401f7c
Abraham Muro <geowall32@gmail.com>
2024-11-24 15:04:23 -0600

    Parcial03 - Add routing to member detail page

--- client/src/app/members/members-detail/members-detail.component.ts
+++ client/src/app/members/members-detail/members-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-members-detail',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './members-detail.component.html',
   styleUrl: './members-detail.component.css'
 })
-export class MembersDetailComponent {
+export class MembersDetailComponent implements OnInit{
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
 
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    const username = this.route.snapshot.paramMap.get("username");
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: (member) => this.member = member
+    });
+  }
 }
