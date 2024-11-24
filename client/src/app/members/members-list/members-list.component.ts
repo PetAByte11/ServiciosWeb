92ab4d82f7df019bfcd3cba620ee15353cd1c0ee
Abraham Muro <geowall32@gmail.com>
2024-11-24 13:15:10 -0600

    Parcial03 - Add member card component

--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
+import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
   selector: 'app-members-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './members-list.component.html',
   styleUrl: './members-list.component.css'
 })
